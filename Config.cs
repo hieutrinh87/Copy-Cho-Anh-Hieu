8043479b021e604fd16844eedae711bec080a927
Trinh Van Hieu <hieutrinh87@gmail.com>
2021-08-24 16:36:51 +0700

    ver0.0

---
+++ Config.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DigitalDataCopy
+{
+    public class Config
+    {
+
+        public string FolderFromTextBox;
+
+        public string SubfolderPrefixTextBox;
+
+        public string SubfolderPostFixTextBox;
+
+        public string xlsFromTextBox;
+
+        public string xlsPrefixTextBox;
+
+        public string xlsPostfixTextBox;
+
+        public string ExcelExtendsionTextBox;
+
+        public string FolderToTextBox;
+
+        public string xlsToTextBox;
+
+        public string PrintSreenFolder;
+
+        public string PrefixScreenFile;
+
+        public string PostfixScreenFile;
+
+        public bool AutoFill;
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,6 +15,9 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json;
+using System.Drawing;
+using System.Diagnostics;
 
 namespace DigitalDataCopy
 {
@@ -23,6 +26,7 @@ namespace DigitalDataCopy
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
@@ -65,6 +69,7 @@ namespace DigitalDataCopy
         {
             try
             {
+                SaveConfig();
                 CopyButton.IsEnabled = false;
                 CopyButton.Content = "COPYING...";
 
@@ -81,8 +86,8 @@ namespace DigitalDataCopy
 
                 Directory.CreateDirectory(targetFolderPath);
 
-                await  Task.Run(() => FileSystem.CopyFile(SourceExcelFile, TargetxcelFile, 
-                                                           UIOption.AllDialogs, UICancelOption.DoNothing));
+                await Task.Run(() => FileSystem.CopyFile(SourceExcelFile, TargetxcelFile,
+                                                          UIOption.AllDialogs, UICancelOption.DoNothing));
 
                 await Task.Run(() => FileSystem.CopyDirectory(SourceFolderPath,
                                                                targetFolderPath,
@@ -165,6 +170,7 @@ namespace DigitalDataCopy
             if (AutoFillExcelNameCheckBox.IsChecked == true)
             {
                 xlsPrefixTextBox.Text = SubfolderPrefixTextBox.Text;
+                PrefixScreenFileTextBox.Text = SubfolderPrefixTextBox.Text;
             }
 
         }
@@ -174,6 +180,7 @@ namespace DigitalDataCopy
             if (AutoFillExcelNameCheckBox.IsChecked == true)
             {
                 xlsPostfixTextBox.Text = SubfolderPostFixTextBox.Text;
+                PostfixScreenFileTextBox.Text = SubfolderPostFixTextBox.Text;
             }
 
         }
@@ -182,5 +189,176 @@ namespace DigitalDataCopy
         {
             MessageBox.Show("If You Need Support, Please Email To hieutrinh87@gmail.com\nThanks", "Question", MessageBoxButton.OK, MessageBoxImage.Information);
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadConfigIfExits();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            SaveConfig();
+        }
+
+        public void SaveConfig()
+        {
+            Config _config = new Config();
+
+            _config.FolderFromTextBox = FolderFromTextBox.Text;
+            _config.SubfolderPrefixTextBox = SubfolderPrefixTextBox.Text;
+            _config.SubfolderPostFixTextBox = SubfolderPostFixTextBox.Text;
+
+            _config.xlsFromTextBox = xlsFromTextBox.Text;
+            _config.xlsPrefixTextBox = xlsPrefixTextBox.Text;
+            _config.xlsPostfixTextBox = xlsPostfixTextBox.Text;
+            _config.ExcelExtendsionTextBox = ExcelExtendsionTextBox.Text;
+
+            _config.FolderToTextBox = FolderToTextBox.Text;
+
+            _config.xlsToTextBox = xlsToTextBox.Text;
+
+            _config.PrintSreenFolder = PrintSreenFolderTextBox.Text;
+            _config.PrefixScreenFile = PrefixScreenFileTextBox.Text;
+            _config.PostfixScreenFile = PostfixScreenFileTextBox.Text;
+
+            _config.AutoFill = (bool)AutoFillExcelNameCheckBox.IsChecked;
+
+            string ConfigStr = JsonConvert.SerializeObject(_config);
+
+            File.WriteAllText("Config.JSON", ConfigStr);
+        }
+
+        public void LoadConfigIfExits()
+        {
+            var ConfigFilePath = "Config.JSON";
+            if (File.Exists(ConfigFilePath))
+            {
+                var CongifStr = File.ReadAllText(ConfigFilePath);
+
+                Config _config = new Config();
+                _config = JsonConvert.DeserializeObject<Config>(CongifStr);
+
+                FolderFromTextBox.Text = _config.FolderFromTextBox;
+                SubfolderPrefixTextBox.Text = _config.SubfolderPrefixTextBox;
+                SubfolderPostFixTextBox.Text = _config.SubfolderPostFixTextBox;
+
+                xlsFromTextBox.Text = _config.xlsFromTextBox;
+                xlsPrefixTextBox.Text = _config.xlsPrefixTextBox;
+                xlsPostfixTextBox.Text = _config.xlsPostfixTextBox;
+                ExcelExtendsionTextBox.Text = _config.ExcelExtendsionTextBox;
+
+                FolderToTextBox.Text = _config.FolderToTextBox;
+
+                xlsToTextBox.Text = _config.xlsToTextBox;
+
+                PrintSreenFolderTextBox.Text = _config.PrintSreenFolder;
+                PrefixScreenFileTextBox.Text = _config.PrefixScreenFile;
+                PostfixScreenFileTextBox.Text = _config.PostfixScreenFile;
+                AutoFillExcelNameCheckBox.IsChecked = _config.AutoFill;
+            }
+        }
+
+        private void AutoFillExcelNameCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            EnableOfDisableTextbox(false);
+        }
+
+
+        private void AutoFillExcelNameCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            EnableOfDisableTextbox(true);
+        }
+        private void EnableOfDisableTextbox(bool Status)
+        {
+            xlsPrefixTextBox.IsEnabled = Status;
+            xlsPostfixTextBox.IsEnabled = Status;
+            PostfixScreenFileTextBox.IsEnabled = Status;
+            PrefixScreenFileTextBox.IsEnabled = Status;
+        }
+
+        private void ScreenFolderButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyOpenFolder(sender, e, PrintSreenFolderTextBox);
+        }
+
+        private void SolButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveConfig();
+                save_ScreenShot_as_File("SOL");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message , "Screen Shot", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void EolButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                SaveConfig();
+                save_ScreenShot_as_File("EOL");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Screen Shot", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
+
+        private void save_ScreenShot_as_File(string FilePreFix)
+
+        {
+            var MainFolder = PrintSreenFolderTextBox.Text.Trim();
+
+            if (Directory.Exists(MainFolder) == false)
+            {
+                throw new Exception($"The Folder Path Not Exists: {MainFolder}");
+            }
+
+            var Subfolder = PrefixScreenFileTextBox.Text.Trim() + PostfixScreenFileTextBox.Text.Trim();
+
+            var FullFolderPath = MainFolder + "\\" + Subfolder;
+
+
+            
+            var FileName = $"{FilePreFix}_{DateTime.Now.ToString("ddMMyyyy_hhmmss")}.png";
+            var FullFileName = $"{MainFolder}\\{Subfolder}\\{FileName}";
+
+            int screenLeft = (int)SystemParameters.VirtualScreenLeft;
+
+            int screenTop = (int)SystemParameters.VirtualScreenTop;
+
+            int screenWidth = (int)SystemParameters.VirtualScreenWidth;
+
+            int screenHeight = (int)SystemParameters.VirtualScreenHeight;
+
+            Bitmap bitmap_Screen = new Bitmap(screenWidth, screenHeight);
+
+            Graphics g = Graphics.FromImage(bitmap_Screen);
+
+            g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap_Screen.Size);
+
+
+            Directory.CreateDirectory(FullFolderPath);
+
+            bitmap_Screen.Save(FullFileName);
+
+            //ProcessStartInfo Info = new ProcessStartInfo()
+            //{
+            //    FileName = "mspaint.exe",
+            //    Arguments = FullFileName
+            //};
+            //Process.Start(Info);
+
+            ProcessStartInfo startInfo = new ProcessStartInfo(FullFileName);
+            startInfo.Verb = "edit";
+
+            Process.Start(startInfo);
+
+        }
+
+
     }
 }
